d1657a2dafe13bf3e839ae388f81b299b1f23dd5
Fariba Shaker <shaker.fariba@gmail.com>
2019-04-09 18:48:21 -0400

    Changed the speed threshold. Now we use mean-std of speed for each link and time slot as the speed threshold.

--- Traffic_data.py
+++ Traffic_data.py
@@ -19,30 +19,40 @@ class Traffic_data:
         self.samples_per_interval = 36
         self.prepare_data()
     def prepare_data(self):
-        pickle_out = open('TrafficData.pkl')
+        #Apr 09-2019: choose the speed threshold automatically using the mean and standard deviation
+        # of speed for each link and each time slot.
+        pickle_out = open('TrafficData.pkl', 'rb')
         speedData = pickle.load(pickle_out)#this is an array of size (43,253,287) (number of days, number of links, samples per day)
         pickle_out.close()
+
+        meanSpeed = np.mean(speedData, axis=0)
+        stdSpeed = np.std(speedData, axis=0)
+        speedThreshold = meanSpeed - stdSpeed # size: (253,287)
         start_time = self.time_period*self.samples_per_interval
         end_time = (self.time_period+1)*self.samples_per_interval
 
         # only use weekdays
         speedData = speedData[
                     [0, 1, 2, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 26, 27, 28, 29, 30, 33, 34, 35,
-                     36, 37, 40, 41, 42], :, start_time: end_time]
-        num_time_points_used = speedData.shape[2]
+                     36, 37, 40, 41, 42], :, :]#start_time: end_time]
+        num_time_points_used = end_time - start_time #speedData.shape[2]
         num_days = speedData.shape[0]
         print(speedData.shape)
         raw_patterns = np.zeros((self.links, num_time_points_used * num_days), float)
         for k in range(num_days):
+            speedData[k,:,:] = (speedData[k,:,:]>speedThreshold).astype(int)
+        speedData = speedData[:,:,start_time: end_time]
+        for k in range(num_days):
             raw_patterns[:, self.samples_per_interval * k: self.samples_per_interval * (k + 1)] = speedData[k, 0:self.links, 0:self.samples_per_interval]
 
         self.train = raw_patterns.T
-        self.train = self.train>self.threshold
+        #self.train = self.train>self.threshold
         self.ds_size = num_time_points_used * num_days
         # Prepare the test data
-        pickle_out = open("test_week_speed_Jan29.pickle", "rb")
+        pickle_out =open("test_week_speed_Jan023.pickle", "rb")
         raw_speed_test = pickle.load(pickle_out)
         pickle_out.close()
-        raw_speed_test = raw_speed_test[0:self.links, start_time:end_time]
-        self.test = raw_speed_test.T
-        self.test = self.test>self.threshold
+        speed_test = raw_speed_test>speedThreshold
+        speed_test = speed_test[0:self.links, start_time:end_time]
+        self.test = speed_test.T
+        #self.test = self.test>self.threshold
